d9f39e10cb7f7c68c18885c1f630eb0e837ee640
siderilla <siderilla11@gmail.com>
2025-01-05 20:36:44 +0100

    printf finito

--- Milestone01/printf/ft_printf.h
+++ Milestone01/printf/ft_printf.h
@@ -1,18 +1,16 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h> // write
-#include <stdio.h> // eventuale uso printf vero per comparazione
-#include <stdarg.h> // macro e typedef va_ ...
-#include <limits.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 size_t	ft_strlen(const char *s);
-void	ft_putunbr_fd(unsigned int n, int fd);
-void	ft_puthex(unsigned int n, int upper);
-void	ft_putptr(void *ptr, int upper);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putchar_fd(char c, int fd);
+ssize_t	ft_putunbr(unsigned int n);
+ssize_t	ft_puthex(unsigned long n, int upper);
+ssize_t	ft_putptr(void *ptr, int upper);
+ssize_t	ft_putnbr(int n);
+ssize_t	ft_putstr(char *s);
+ssize_t	ft_putchar(char c);
 
 #endif
